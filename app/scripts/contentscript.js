c81de252e05f8a57add1077f23773002661b9874
Shu Uesugi <shu@chibicode.com>
2014-11-05 18:14:59 -0800

    Write first version

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,3 +1,43 @@
-'use strict';
+/*global jQuery:false */
+(function($) {
+  'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+  var recalculatePoints = function() {
+    $('.zh-pipeline').each(function() {
+      var sum = $(this)
+        .find('.zh-issue-label')
+        .map(function() {
+          var labelName = $(this).data('name');
+          if ($.isNumeric(labelName)) {
+            return parseFloat(labelName);
+          } else {
+            return 0;
+          }
+        })
+        .get()
+        .reduce(function(a, b) {
+          return a + b;
+        }, 0);
+
+      $(this)
+        .find('.zh-pipeline-count')
+        .text(sum)
+        .addClass('zh-scrum-points-calculated');
+    });
+  };
+
+  // Wait for save event by watching the class property of `.zh-board-pipelines`
+  var installSaveObserver = function() {
+    var target = $('.zh-board-pipelines')[0];
+    var watchedAttributes = ['class'];
+    var observer = new MutationObserver(recalculatePoints);
+    observer.observe(target, { attributes: true, attributeFilter: watchedAttributes });
+  };
+
+  // Wait for `.zh-board` to be in the DOM
+  $('body')
+    .livequery('.zh-board', function() {
+      installSaveObserver();
+      recalculatePoints();
+    });
+})(jQuery);
